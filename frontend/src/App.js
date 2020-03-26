e4dfc54e916bd914c728d8e7f3362470b0a4d6c6
Renato Neri Jr <renatonerijr@gmail.com>
2020-03-26 10:24:13 -0300

    Create Register, Logon and Profile, Profile is not styled entirely yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from 'react'
+
+import Routes from './routes'
+import './global.css'
 
 function App() {
+
   return (
-    <h1>Hello World</h1>
+      <Routes />
   );
 }
 
-export default App;
+export default App
---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react'
+
+import Logon from './pages/Logon'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
+
+export default function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
